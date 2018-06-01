58f476c4a799c0c8a4500a2b274e7f7cd4c8bdd2
Lisa Stillwell <lisa@renci.org>
2018-06-01 12:30:43 -0400

    make updates to support designed changes as discussed at 5-31-18 whiteboard session

--- asgs-msgr/asgs-msgr.py
+++ asgs-msgr/asgs-msgr.py
@@ -7,37 +7,35 @@ import json
 
 def usage():
 	print('\nUsage:\n')
-	print('send-msg.py -h -u <Uid> -l <LocationName> -c <ClusterName> -t <RunType> -d <UTCDateTime> -s <StormName> -n <StormNumber> -a <AdvisoryNumber> -m <Message> -y <MessageType')
+	print('send-msg.py -h -l <LocationName> -c <ClusterName>  -d <UTCDateTime> -s <StormName> -n <StormNumber> -a <AdvisoryNumber> -m <Message> -y <EventType> -p <Process> -t <PctComplete> -r <State>')
 	print('		')		
 	print(' where -h | --Help		the text you are looking at right now')
-	print('       -u | --Uid		a unique id for this particular model run')
 	print('       -l | --LocationName	name of location where model is running, i.e. UNC')
 	print('       -c | --ClusterName 	name of cluster running model, i.e. Hatteras')
-	print('       -t | --RunType		hurricane | weather')
 	print('       -d | --UTCDateTime	UTC date time - ISO 8601 format; if not provided defaults to now')
 	print('       -s | --StormName		hurricane name, i.e. Irene')
 	print('       -n | --StormNumber 	hurricane id number')
 	print('       -a | --AdvisoryNumber	NHC advisory number for this run')
 	print('       -m | --Message		the actual message to send')
-	print('       -p | --MessageType	message type, i.e. startup | info | warning | error, etc')
+	print('       -y | --EventType		event type, RSTR | PRE1 | NOWC | PRE2 | FORC | POST | REND ')
+	print('       -p | --Process		software process issuing this message')
+	print('       -t | --PctComplete	numeric percentage of completion of process running, i.e. 34.2')
+	print('       -r | --State		ASGS run state, STRT | RUNN | PEND | FAIL | WARN | IDLE | EXIT')
 	print(' ')
 
-def JsonifyMessage(Uid,
-                   LocationName,
+def JsonifyMessage(LocationName,
                    ClusterName,
-                   RunType,
                    UTCDateTime,
                    StormName,
                    StormNumber,
                    AdvisoryNumber,
                    Message,
-                   MessageType):
+                   EventType,
+                   Process,
+                   PctComplete,
+                   State):
 	
-	if (RunType == 'hurricane'):
-		hurr_obj = {'storm': StormName, 'storm_number': StormNumber, 'advisory_number': AdvisoryNumber}
-		main_obj = {'name': 'asgs', 'uid': Uid, 'physical_location': LocationName, 'clustername': ClusterName, 'run_type': RunType, 'date-time': UTCDateTime, 'hurricane': hurr_obj, 'message': Message, 'message_type': MessageType}
-	else:
-		main_obj = {'name': 'asgs', 'uid': Uid, 'physical_location': LocationName, 'clustername': ClusterName, 'run_type': RunType, 'date-time': UTCDateTime, 'message': Message, 'message_type': MessageType}
+	msg_obj = {'name': 'asgs', 'physical_location': LocationName, 'clustername': ClusterName, 'date-time': UTCDateTime, 'message': Message, 'event_type': EventType, 'process': Process, 'pctcomplete': PctComplete, 'state': State, 'storm': StormName, 'storm_number': StormNumber, 'advisory_number': AdvisoryNumber}
 
 	return json.dumps(main_obj)
 
@@ -58,20 +56,21 @@ def queue_message(message):
 
 def main(argv):
 
-	Uid = ''
-	LocationName = ''
-	ClusterName = ''
-	RunType = ''
+	LocationName = 'RENCI'
+	ClusterName = 'hatteras'
 	tmpDateTime = datetime.datetime.utcnow()
 	UTCDateTime = tmpDateTime.strftime("%Y-%m-%d %H:%M:%S")
 	StormName = ''
 	StormNumber = ''
 	AdvisoryNumber = ''
 	Message = 'test message'
-	MessageType = ''
+	EventType = ''
+	Process = 'asgs'
+	PctComplete = '0'
+	State = 'IDLE'
 
 	try:
-        	opts, args = getopt.getopt(argv,"hu:l:c:t:d:s:n:a:m:p:",["Help","Uid=","LocationName=","ClusterName=","RunType=","UTCDateTime=","StormName=", "StormNumber=", "AdvisoryNumber=", "Message=", "MessageType="])
+        	opts, args = getopt.getopt(argv,"hl:c:d:s:n:a:m:y:p:t:r:",["Help","LocationName=","ClusterName=","UTCDateTime=","StormName=", "StormNumber=", "AdvisoryNumber=", "Message=", "EventType=", "Process=", "PctComplete=", "State="])
 	except getopt.GetoptError as err:
         	print('\nCommand line option error: ' + str(err))
         	usage()
@@ -81,14 +80,10 @@ def main(argv):
 		if opt in ("-h", "--Help"):
 			usage()
 			sys.exit(2)
-		elif opt in ("-u", "--Uid"):
-			Uid = arg
 		elif opt in ("-l", "--LocationName"):
 			LocationName = arg
 		elif opt in ("-c", "--ClusterName"):
 			ClusterName = arg
-		elif opt in ("-t", "--RunType"):
-			RunType = arg
 		elif opt in ("-d", "--UTCDateTime"):
 			UTCDateTime = arg
 		elif opt in ("-s", "--StormName"):
@@ -99,20 +94,27 @@ def main(argv):
 			AdvisoryNumber = arg     
 		elif opt in ("-m", "--Message"):
 			Message = arg
-		elif opt in ("-p", "--MessageType"):
-			MessageType = arg
+		elif opt in ("-y", "--EventType"):
+			EventType = arg
+		elif opt in ("-p", "--Process"):
+			Process = arg
+		elif opt in ("-t", "--PctComplete"):
+			PctComplete = arg
+		elif opt in ("-r", "--State"):
+			State = arg
 
 	msg = JsonifyMessage(
-			Uid, 
 			LocationName, 
 			ClusterName, 
-			RunType, 
 			UTCDateTime, 
 			StormName, 
 			StormNumber, 
 			AdvisoryNumber, 
 			Message, 
-			MessageType
+			MessageType,
+                        Process,
+                        PctComplete,
+                        State
 			)
 
 	queue_message(msg)
